ab2960547c79ae54e3c98f27609f44be82c680d3
Britton Ballard <davidbritton.ballard@gmail.com>
2012-09-07 20:56:03 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameOfThrones::Application.config.secret_token = 'b6512f9160f7d19fcad36d53aac8c5561e463078fa5c9490d873d6e90d3b514719e195360a1cc8aa6334cadc426b1f437cf27fa714e2f2ca7e12ddffc7d80e77'
