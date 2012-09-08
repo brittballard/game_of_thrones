217054b7a935f9a9f237a7101badbb307168b34e
Britton Ballard <davidbritton.ballard@gmail.com>
2012-09-08 00:24:37 -0500

    Organized the specs better and removed the test folder.

--- spec/models/charecter_spec.rb
+++ spec/models/charecter_spec.rb
@@ -9,64 +9,70 @@ describe Charecter, '#children' do
 	end
 end
 
-describe Charecter, '#validate_parent_is_not_a_child' do
+describe Charecter, 'validation' do
 	before :each do
 		@ned = FactoryGirl.create(:charecter)
 		@arya = FactoryGirl.build(:charecter, first_name: "Arya", last_name: "Stark")
 	end
+	
+	describe Charecter, '#validate_parent_is_not_a_child' do
+		it 'should not allow a charecter to be saved with the same model as a mother and a child' do
+			@arya.father = @ned
+			@arya.save!
 
-	it 'should not allow a charecter to be saved with the same model as a mother and a child' do
-		@arya.father = @ned
-		@arya.save!
+			@ned.mother = @arya
 
-		@ned.mother = @arya
+			@ned.valid?
 
-		@ned.valid?
+			@ned.errors.size.should == 1
+	  	@ned.errors.messages[:mother].should == ["A charecter's child cannot also be its mother"]
+	  end
 
-		@ned.errors.size.should == 1
-  	@ned.errors.messages[:mother].should == ["A charecter's child cannot also be its mother"]
-  end
+	  it 'should not allow a charecter to be saved with the same model as a father and a child' do
+			@arya.father = @ned
+			@arya.save!
 
-  it 'should not allow a charecter to be saved with the same model as a father and a child' do
-		@arya.father = @ned
-		@arya.save!
+			@ned.father = @arya
 
-		@ned.father = @arya
+			@ned.valid?
 
-		@ned.valid?
+			@ned.errors.size.should == 1
+	  	@ned.errors.messages[:father].should == ["A charecter's child cannot also be its father"]
+	  end
+	end
 
-		@ned.errors.size.should == 1
-  	@ned.errors.messages[:father].should == ["A charecter's child cannot also be its father"]
-  end
+	describe Charecter, '#validate_mother_and_father_are_not_the_same_charecter' do
+	  it 'should not allow a charecter to be saved with the same charecter as its mother and father' do
+			@arya.save!
 
-  it 'should not allow a charecter to be saved with the same charecter as its mother and father' do
-		@arya.save!
+			@ned.father = @arya
+			@ned.mother = @arya
 
-		@ned.father = @arya
-		@ned.mother = @arya
+			@ned.valid?
 
-		@ned.valid?
+			@ned.errors.size.should == 2
+	  	@ned.errors.messages[:mother].should == ["A charecter's mother and father cannot be the same charecter"]
+	  	@ned.errors.messages[:father].should == ["A charecter's mother and father cannot be the same charecter"]
+	  end
+	end
 
-		@ned.errors.size.should == 2
-  	@ned.errors.messages[:mother].should == ["A charecter's mother and father cannot be the same charecter"]
-  	@ned.errors.messages[:father].should == ["A charecter's mother and father cannot be the same charecter"]
-  end
+	describe Charecter, '#validate_that_a_charecter_is_not_its_own_parent' do
+	  it 'should not allow a charecter to be its own father' do
+			@ned.father = @ned
 
-  it 'should not allow a charecter to be its own father' do
-		@ned.father = @ned
+			@ned.valid?
 
-		@ned.valid?
+			@ned.errors.size.should == 1
+	  	@ned.errors.messages[:father].should == ["A charecter cannot be its own father"]
+	  end
 
-		@ned.errors.size.should == 1
-  	@ned.errors.messages[:father].should == ["A charecter cannot be its own father"]
-  end
+		it 'should not allow a charecter to be its own mother' do
+			@ned.mother = @ned
 
-	it 'should not allow a charecter to be its own mother' do
-		@ned.mother = @ned
+			@ned.valid?
 
-		@ned.valid?
-
-		@ned.errors.size.should == 1
-  	@ned.errors.messages[:mother].should == ["A charecter cannot be its own mother"]
-  end
+			@ned.errors.size.should == 1
+	  	@ned.errors.messages[:mother].should == ["A charecter cannot be its own mother"]
+	  end
+	end
 end
